c5541c18c86dbe403cb6929683d605e00490ed3f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-08 20:03:44 -0300

    failocr

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from preprocess import preprocessar
 from ocr import extrair_texto
 
-# processar imagem
 img = preprocessar('foto-caderno.jpeg', salvar=True)
 
-# extrair texto
 texto = extrair_texto(img)
 
-# 🔥 salvar resultado
 with open('resultado.txt', 'w', encoding='utf-8') as f:
     f.write(texto)
 
